3f7639055ec229509d1ae71b49829130a6009669
Neha jaiswal <jneha1868@gmail.com>
2025-01-23 16:58:59 +0530

    Indexfile added

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv"
+dotenv.config()
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        const port = process.env.PORT || 8000;
+        
+        app.listen(port, () => {
+            console.log(`Server is running at port: ${port}`);
+        });
+    } catch (error) {
+        console.error("Error starting server:", error);
+    }
+};
+
+startServer();
+
